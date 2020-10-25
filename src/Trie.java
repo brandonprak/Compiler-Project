0e47f138794eed7959becb8fc6f0b89301db7f36
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2020-10-24 22:35:26 -0700

    Update Trie.java
    Added comments+javadoc to Trie.java

--- src/Trie.java
+++ src/Trie.java
@@ -1,15 +1,18 @@
 import java.util.*;
 import java.util.regex.Pattern;
 
-
+/**
+ *Basic trie implementation of a table.
+ *
+ *<p>Keys are {@code String}s matching the regular expression [A-Za-z][A-Za-z0-9]*
+ *
+ *@param <V> Type of mapped values
+ */
 public class Trie<V>{
 	
 	private static final Pattern KEY_PATTERN = Pattern.compile("[A-Za-z][A-Za-z0-9]*");
-	
-	private static final char EMPTY = '.', SEPARATOR = '*', VALUE = '#';
-	
-	private static final int DEFAULT_INIT_SSIZE = 512, DEFAULT_INIT_VSIZE = 16;
-	
+	private static final char    EMPTY = '.', SEPARATOR = '*', VALUE = '#';
+	private static final int     DEFAULT_INIT_SSIZE = 512, DEFAULT_INIT_VSIZE = 16;
 	
 	private int[] switchArray, next;
 	private char[] symbol;
@@ -17,6 +20,14 @@ public class Trie<V>{
 	
 	private int sSize, vSize, nextIndex, currentValue, count;
 	
+	/**
+	 *Constructs a trie table with the given sizes of the symbol and value arrays.
+	 *
+	 *@param sSize Size of the symbol and next arrays. Must be greater than 0.
+	 *@param vSize Size of the value array. Must be greater than 0.
+	 *
+	 *@throws IllegalArgumentException If either sSize or vSize are less than or equal to 0.
+	 */
 	public Trie(int sSize, int vSize){
 		if(sSize <= 0)
 			throw new IllegalArgumentException("sSize must be >0");
@@ -43,19 +54,45 @@ public class Trie<V>{
 		Arrays.fill(symbol, EMPTY);
 	}
 	
+	/**
+	 *Constructs a trie table with the default sizes of the symbol and value arrays.
+	 */
 	public Trie(){
 		this(DEFAULT_INIT_SSIZE, DEFAULT_INIT_VSIZE);
 	}
 	
-	public int reserve(String s){
+	/**
+	 *Reserves a key-value mapping for future use. This should be the primary method of adding a key to the table in order to map a value to it later.
+	 *
+	 *<p>Reserving a previously existing key has no effect.
+	 *
+	 *@param s The key to reserve.
+	 *
+	 *@throws IllegalArgumentException If the key being reserved does not match the regular expression given above.
+	 */
+	public void reserve(String s){
+		reserve(s, 0);
+	}
+	
+	private int reserve(String s, int unused /*to overload method*/){
 		if(!matches(KEY_PATTERN, s))
-			throw new RuntimeException("Invalid key: " + s);
+			throw new IllegalArgumentException("Invalid key: " + s);
 		
 		return search(s + SEPARATOR, true);
 	}
 	
+	/**
+	 *Mapps a value to a given key, reserving it if necessary. 
+	 *
+	 *<p>Mapping a previously mapped value overwrites the existing value.
+	 *
+	 *@param s The key to map to.
+	 *@param value The value to map. May be {@code null}.
+	 *
+	 *@throws IllegalArgumentException If the key being mapped to does not match the regular expression given above.
+	 */
 	public void put(String s, V value){
-		int index = reserve(s);
+		int index = reserve(s, 0);
 		
 		index = next[index];
 		
@@ -65,6 +102,14 @@ public class Trie<V>{
 		values[index] = value;
 	}
 	
+	/**
+	 *Retrieves the value for a given key, or {@code null} if it does not exist. 
+	 *
+	 *@param s The key to search for.
+	 *@return The value of the key, or {@code null} if it does not exist. 
+	 *
+	 *@throws IllegalArgumentException If the key is {@code null}.
+	 */
 	public V get(String s){
 		if(s == null)
 			throw new IllegalArgumentException(s);
@@ -79,6 +124,12 @@ public class Trie<V>{
 			return null;
 	}
 	
+	/**
+	 *Checks if the table contains a mapping for a given key.
+	 *
+	 *@param s The key to search for.
+	 *@return {@code true} if the key is present in the table, {@code false} if not. Also {@code false} if s is an invalid key or {@code null}.
+	 */
 	public boolean contains(String s){
 		return (matches(KEY_PATTERN, s)) ? search(s + SEPARATOR, false) != -1 : false;
 	}
@@ -103,6 +154,8 @@ public class Trie<V>{
 		}
 	} */
 	
+	//Searches the arrays for the index of the value pointer for the key s. If create is true, the key is created and the index of its value pointer is returned;
+	//if create is false, -1 is returned.
 	private int search(String s, boolean create){
 		char c;
 		int i = 0;
@@ -110,10 +163,9 @@ public class Trie<V>{
 		c = s.charAt(i++);
 		
 		boolean notFound = false;
+		boolean inSwitch = true;
 		
 		int ptr = switchArray[switchChar(c)];
-		
-		boolean inSwitch = true;
 		
 		if(ptr == -1)
 			notFound = true;
@@ -178,13 +230,17 @@ public class Trie<V>{
 		return ptr - 1;
 	}
 	
+	/**
+	 *Prints the contents of the arrays representing the trie.
+	 *
+	 *@return A string representation of the trie.
+	 */
 	@Override
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
-		
 		StringBuilder line1 = null, line2 = null, line3 = null;
 		
-		int max = 74;
+		final int max = 74;
 		
 		int l = 0;
 		
@@ -242,6 +298,7 @@ public class Trie<V>{
 		return sb.deleteCharAt(sb.length() - 2).toString();
 	}
 	
+	//Grows (doubles) the symbol and next arrays.
 	private void growSymbolArray(){
 		int newSize = sSize << 1;
 		int[] newNext = new int[newSize];
@@ -256,12 +313,13 @@ public class Trie<V>{
 		sSize = newSize;
 	}
 	
+	//Grows (doubles) the value array.
 	private void growValueArray(){
 		int newSize = vSize << 1;
 		
 		@SuppressWarnings("unchecked")
 		V[] newValue = (V[])new Object[newSize];
-
+		
 		System.arraycopy(values, 0, newValue, 0, vSize);
 		
 		values = newValue;
@@ -269,7 +327,7 @@ public class Trie<V>{
 		vSize = newSize;
 	}
 	
-	
+	//Returns the index in the switch array of a given char.
 	private static int switchChar(char c){
 		if(c >= 'A' && c <= 'Z')
 			return c - 'A';
@@ -278,6 +336,7 @@ public class Trie<V>{
 		else return -1;
 	}
 	
+	//Convenience method to compare a string against a regular expression.
 	private static boolean matches(Pattern p, String s){
 		return (p != null && s != null) ? p.matcher(s).matches() : false;
 	}
